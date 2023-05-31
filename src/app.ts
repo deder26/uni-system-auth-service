bd2c2460c927d956c4a9cf098059040a8ef373dc
deder26 <dederhossain24@gmail.com>
2023-05-31 23:52:06 +0900

    eslint prettier husky added

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
-const app:Application = express();
+const app: Application = express()
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
 
-app.get('/', (req:Request, res:Response) => {
-    res.send('Hello World!')
-  })
-  
- export default app;
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.get('/', (req: Request, res: any) => {
+  res.send('Hello World!')
+})
+
+export default app
